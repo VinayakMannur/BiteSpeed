8aced340c0963436f5785c94a52debe9501e9b0c
VinayakMannur <Vinayakmannur20@gmail.com>
2024-02-08 19:27:57 +0530

    serving html file for frontend

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const bodyParser = require("body-parser");
 const sequelize = require("./utils/database");
 const contactRoutes = require("./routes/contactRoute");
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(contactRoutes);
 
---
+++ public/script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitButton = document.getElementById("submit");
+  const responseModalBody = document.getElementById("responseModalBody");
+
+  submitButton.addEventListener("click", async function () {
+    const email = document.getElementById("email").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+
+    try {
+      const response = await fetch("http://localhost:5000/identify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          phoneNumber: phoneNumber,
+        }),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        // console.log(result);
+        document.getElementById("email").value = '';
+        document.getElementById("phoneNumber").value = '';
+
+
+        responseModalBody.innerHTML = `
+                <p>Primary Contact ID: ${result.contact.primaryContatctId}</p>
+                <p>Emails: ${result.contact.emails.join(", ")}</p>
+                <p>Phone Numbers: ${result.contact.phoneNumbers.join(", ")}</p>
+                <p>Secondary Contact IDs: ${result.contact.secondaryContactIds.join(
+                  ", "
+                )}</p>
+                `;
+
+        const responseModal = new bootstrap.Modal(
+          document.getElementById("responseModal")
+        );
+        responseModal.show();
+      } else {
+        console.error("Error:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  });
+});
